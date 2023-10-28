0742b5ad59883cd46fc327c905daeda374bb26f4
tinokoci <valentino.reformed@gmail.com>
2023-10-28 20:26:57 +0200

    Latest

--- server/src/main/kotlin/dev/valentino/echo/server/EchoServer.kt
+++ server/src/main/kotlin/dev/valentino/echo/server/EchoServer.kt
@@ -1,5 +1,17 @@
 package dev.valentino.echo.server
 
-fun main() {
-    println("hello from server")
+import java.net.ServerSocket
+
+class EchoServer {
+
+    private val port = 10000;
+
+    fun main(args: Array<String>) {
+        println(args);
+        val serverSocket = ServerSocket(port);
+        println("Started server on port $port");
+    }
+
+    fun getPortFromArgs(args: Array<String>) {
+    }
 }
